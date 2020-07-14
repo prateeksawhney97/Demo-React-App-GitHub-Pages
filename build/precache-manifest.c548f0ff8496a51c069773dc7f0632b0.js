0f51203149c7e740242dd9e8972b0970735fd10b
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2020-07-14 21:28:08 +0530

    done

---
+++ build/precache-manifest.c548f0ff8496a51c069773dc7f0632b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5b037dd565283d8fd0fd36f43325e6",
+    "url": "/Youtube-Video-Browser/index.html"
+  },
+  {
+    "revision": "607c9c69a75b42ce6360",
+    "url": "/Youtube-Video-Browser/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a40ed2ac08b18711fbff",
+    "url": "/Youtube-Video-Browser/static/js/2.84a58295.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Youtube-Video-Browser/static/js/2.84a58295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "607c9c69a75b42ce6360",
+    "url": "/Youtube-Video-Browser/static/js/main.7ea73dd0.chunk.js"
+  },
+  {
+    "revision": "b947479ea29e46f26b49",
+    "url": "/Youtube-Video-Browser/static/js/runtime-main.f61d8695.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Youtube-Video-Browser/static/media/logo.5d5d9eef.svg"
+  }
+]);
